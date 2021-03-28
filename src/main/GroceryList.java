f62e101de6b0d06fd2658c6364f3d5d0d1308017
cjabeyakaran <cjabeyakaran@wustl.edu>
2021-03-28 09:30:57 -0500

    Completed function to calculate total cost per person

--- src/main/GroceryList.java
+++ src/main/GroceryList.java
@@ -2,6 +2,9 @@ package main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
 
 public class GroceryList {
 	private List<GroceryItem> items;
@@ -12,6 +15,17 @@ public class GroceryList {
 	
 	public boolean addItem(GroceryItem item) {
 		return this.items.add(item);
+	}
+
+	public Map<String, Float> getAmountOwed() {
+		Map<String, Float> perPerson = new HashMap<String, Float>();
+		for (GroceryItem item: items) {
+			Set<String> consumers = item.getConsumers();
+			for (String person: consumers) {
+				perPerson.put(person, perPerson.getOrDefault(person, (float) 0) + item.getPricePerConsumer());
+			}
+		}
+		return perPerson;
 	}
 	
 	@Override
